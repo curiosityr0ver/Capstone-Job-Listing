9a528b0d097405c5cb3d672974e8ccc241897c98
Ishu Mehta <mehta.ishu14@gmail.com>
2024-06-24 19:21:09 +0530

    responsive UI

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,17 +1,16 @@
-/* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 import { Login } from "../api/User";
 import { Navigate } from "react-router-dom";
+import styles from "./LoginPage.module.css";
 
 function LoginPage({ setCurrentUser }) {
-	const [email, setEmail] = useState();
-	const [password, setPassword] = useState();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 	const [redirectToHome, setRedirectToHome] = useState(false);
 
 	useEffect(() => {
 		const storedEmail = localStorage.getItem("email");
 		const storedPassword = localStorage.getItem("password");
-		console.log(storedEmail, storedPassword);
 		if (storedEmail) {
 			setEmail(storedEmail);
 		}
@@ -22,35 +21,46 @@ function LoginPage({ setCurrentUser }) {
 
 	const handleLogin = async () => {
 		const response = await Login(email, password);
-		console.log(response);
-		if (response.status == 200) {
+		if (response.status === 200) {
 			setCurrentUser(true);
 			localStorage.setItem("token", response.data.token);
 			setRedirectToHome(true);
 		}
 	};
 
-	useEffect(() => {
-		console.log(redirectToHome);
-	}, [redirectToHome]);
-
 	return (
-		<div>
-			<h1>Login</h1>
-			<input
-				type="email"
-				placeholder="Email"
-				value={email}
-				onChange={(e) => setEmail(e.target.value)}
-			/>
-			<input
-				type="password"
-				placeholder="Password"
-				value={password}
-				onChange={(e) => setPassword(e.target.value)}
-			/>
-			<button onClick={handleLogin}>Login</button>
-			{redirectToHome && <Navigate to="/" />}
+		<div className={styles.container}>
+			<div className={styles.leftSide}>
+				<h1 className={styles.header}>Already have an account?</h1>
+				<p className={styles.subheader}>Your personal job finder is here</p>
+				<input
+					type="email"
+					placeholder="Email"
+					value={email}
+					className={styles.input}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					value={password}
+					className={styles.input}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<button className={styles.button} onClick={handleLogin}>
+					Login
+				</button>
+				<div
+					className={styles.signupRedirect}
+					onClick={() => (window.location.href = "/signup")}
+				>
+					Don't have an account? Sign Up
+				</div>
+				{redirectToHome && <Navigate to="/" />}
+			</div>
+			<div className={styles.rightSide}>
+				<h1>Your personal job finder</h1>
+			</div>
 		</div>
 	);
 }
